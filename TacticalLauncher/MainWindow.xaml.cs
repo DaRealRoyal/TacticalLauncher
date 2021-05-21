38b5bd04db00c64a8f8893e12d0a4ad0c266c946
nalsai <contact@nalsai.de>
2021-05-21 15:42:26 +0200

    made a basic launcher

---
+++ TacticalLauncher/MainWindow.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace TacticalLauncher
+{
+    enum LauncherStatus
+    {
+        ready,
+        failed,
+        downloadingGame,
+        downloadingUpdate
+    }
+
+    public partial class MainWindow : Window
+    {
+        const string downloadUrlVersion = "https://drive.google.com/uc?export=download&id=18FkOPeDDzqPPgmRb4XdzEHEchfM5U3HV";
+        const string downloadUrlGame = "https://drive.google.com/uc?export=download&id=1Ts8BzGfmp_JF_XTchyQMPjiIT90PDKGk";
+
+        string rootPath;
+        string versionFile;
+        string gameZip;
+        string gameExe;
+
+        LauncherStatus _status;
+        internal LauncherStatus Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                switch (_status)
+                {
+                    case LauncherStatus.ready:
+                        PlayButton.Content = "Play";
+                        break;
+                    case LauncherStatus.failed:
+                        PlayButton.Content = "Update Failed - Retry";
+                        break;
+                    case LauncherStatus.downloadingGame:
+                        PlayButton.Content = "Downloading Game...";
+                        break;
+                    case LauncherStatus.downloadingUpdate:
+                        PlayButton.Content = "Downloading Update...";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            VersionTextLauncher.Text = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            rootPath = Directory.GetCurrentDirectory();
+            versionFile = Path.Combine(rootPath, "Version.txt");
+            gameZip = Path.Combine(rootPath, "TMR.zip");
+            gameExe = Path.Combine(rootPath, "TMR", "TacticalMathReturns.exe");
+        }
+
+        private void CheckForUpdates()
+        {
+            if (File.Exists(versionFile))
+            {
+                Version localVersion = new Version(File.ReadAllText(versionFile));
+                VersionTextGame.Text = localVersion.ToString();
+
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    Version onlineVersion = new Version(webClient.DownloadString(downloadUrlVersion).TrimStart('v'));
+
+                    if (onlineVersion != localVersion)
+                    {
+                        InstallGame(true, onlineVersion);
+                    }
+                    else
+                    {
+                        Status = LauncherStatus.ready;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Status = LauncherStatus.failed;
+                    MessageBox.Show($"Error checking for updates: {ex}");
+                }
+            }
+            else
+            {
+                InstallGame(false, new Version(0, 0));
+            }
+            PlayButton.IsEnabled = true;
+        }
+
+        private void InstallGame(bool _isUpdate, Version _onlineVersion)
+        {
+            try
+            {
+                WebClient webClient = new WebClient();
+                if (_isUpdate)
+                {
+                    Status = LauncherStatus.downloadingUpdate;
+                }
+                else
+                {
+                    Status = LauncherStatus.downloadingGame;
+                    _onlineVersion = new Version(webClient.DownloadString(downloadUrlVersion).TrimStart('v'));
+                }
+
+                FileDownloader fileDownloader = new FileDownloader();
+                fileDownloader.DownloadProgressChanged += (sender, e) => Progress.Text = "Progress: " + e.BytesReceived + "/" + e.TotalBytesToReceive;
+                fileDownloader.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
+                fileDownloader.DownloadFileAsync(downloadUrlGame, gameZip, _onlineVersion);
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error downloading game: {ex}");
+            }
+        }
+
+        private void DownloadGameCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            Progress.Text = "";
+            try
+            {
+                string onlineVersion = ((Version)e.UserState).ToString();
+                ZipFile.ExtractToDirectory(gameZip, rootPath, true);
+                File.Delete(gameZip);
+
+                File.WriteAllText(versionFile, onlineVersion);
+
+                VersionTextGame.Text = onlineVersion;
+                Status = LauncherStatus.ready;
+            }
+            catch (Exception ex)
+            {
+                Status = LauncherStatus.failed;
+                MessageBox.Show($"Error installing game: {ex}");
+            }
+        }
+
+        private void Window_ContentRendered(object sender, EventArgs e)
+        {
+            CheckForUpdates();
+        }
+
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(gameExe) && Status == LauncherStatus.ready)
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo(gameExe);
+                startInfo.WorkingDirectory = Path.Combine(rootPath, "TMR");
+                Process.Start(startInfo);
+
+                Close();
+            }
+            else if (Status == LauncherStatus.failed)
+            {
+                CheckForUpdates();
+            }
+        }
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
+        {
+            var processStartInfo = new ProcessStartInfo()
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            };
+            Process.Start(processStartInfo);
+
+            e.Handled = true;
+        }
+    }
+}
