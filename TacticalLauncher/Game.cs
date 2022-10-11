9750247e67ba454295d2935e5b4a9a08ab3e2348
Nils Fürniß <contact@nalsai.de>
2022-10-11 20:41:38 +0200

    Add multi game support

---
+++ TacticalLauncher/Game.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace TacticalLauncher
+{
+    public enum GameState
+    {
+        start,
+        clickPlay,
+        clickUpdate,
+        clickInstall,
+        downloading,
+        installing,
+        failed
+    }
+
+    class Game : INotifyPropertyChanged
+    {
+        static readonly string launcherPath = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).ToString());
+        static readonly string settingsPath = Path.Combine(launcherPath, "settings.xml");   // TODO
+        static readonly string gamesPath = Path.Combine(launcherPath, "Games");
+        static readonly string downloadPath = Path.Combine(gamesPath, "Downloads");
+
+        readonly string versionFile;
+        readonly string gameName;
+        readonly string gamePath;
+        readonly string downloadUrl;
+        readonly string downloadVersionUrl;
+        readonly string gameExe;
+
+        private GameState _state;
+        public GameState State
+        {
+            get { return _state; }
+            set
+            {
+                _state = value;
+                RaisePropertyChanged();
+                RaisePropertyChanged(nameof(StatusText));
+                RaisePropertyChanged(nameof(IsReady));
+                RaisePropertyChanged(nameof(IsReadyOrFailed));
+            }
+        }
+
+        public string StatusText
+        {
+            get
+            {
+                switch (_state)
+                {
+                    case GameState.start:
+                        return "Checking For Updates...";
+                    case GameState.clickPlay:
+                        return "Play";
+                    case GameState.clickUpdate:
+                        return "Update";
+                    case GameState.clickInstall:
+                        return "Install";
+                    case GameState.failed:
+                        return "Failed - Retry?";
+                    case GameState.downloading:
+                        return "Downloading...";
+                    case GameState.installing:
+                        return "Installing...";
+                }
+                return _state.ToString();
+            }
+        }
+
+        public bool IsReady =>
+            _state == GameState.clickPlay ||
+            _state == GameState.clickUpdate ||
+            _state == GameState.clickInstall;
+
+        public bool IsReadyOrFailed =>
+            IsReady || _state == GameState.failed;
+
+        private long _downloadSize;
+        public long DownloadSize
+        {
+            get { return _downloadSize; }
+            set
+            {
+                _downloadSize = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private long _downloadSizeCurrent;
+        public long DownloadSizeCurrent
+        {
+            get { return _downloadSizeCurrent; }
+            set
+            {
+                _downloadSizeCurrent = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private string _progressText;
+        public string ProgressText
+        {
+            get { return _progressText; }
+            set
+            {
+                _progressText = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        private Version _localVersion;
+        public Version LocalVersion
+        {
+            get { return _localVersion; }
+            set
+            {
+                _localVersion = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        /// <summary>
+        /// Creates instance of the Game
+        /// </summary>
+        /// <param name="url">Download URL of the game's .zip file</param>
+        /// <param name="versionUrl">Download URL of the text file containing the current verison string of the game</param>
+        /// <param name="name">Name of the game (.zip has to contain a folder with the name)</param>
+        /// <param name="exe">Name of the game's .exe file  (.zip has to contain the exe in the folder)</param>
+        public Game(string url, string versionUrl, string name, string exe)
+        {
+            gameName = name;
+            gamePath = Path.Combine(gamesPath, gameName);
+            gameExe = Path.Combine(gamePath, exe);
+            downloadUrl = url;
+            downloadVersionUrl = versionUrl;
+            versionFile = Path.Combine(gamesPath, name + ".version");
+
+            CheckUpdates();
+        }
+
+        public void CheckUpdates()
+        {
+            if (File.Exists(versionFile) && File.Exists(gameExe))
+            {
+                LocalVersion = new Version(File.ReadAllText(versionFile));
+
+                try
+                {
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(VersionCallback);
+                    webClient.DownloadStringAsync(new Uri(downloadVersionUrl));
+                }
+                catch (Exception ex)
+                {
+                    State = GameState.failed;
+                    MessageBox.Show($"Error checking for updates: {ex}");
+                }
+            }
+            else
+                State = GameState.clickInstall;
+        }
+
+        private void VersionCallback(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                Version onlineVersion = new Version(e.Result.TrimStart('v'));
+                State = onlineVersion == _localVersion ? GameState.clickPlay : GameState.clickUpdate;
+            }
+            catch (Exception ex)
+            {
+                State = GameState.failed;
+                MessageBox.Show($"Error checking for updates: {ex}");
+            }
+        }
+
+        private void DownloadGame(Version _onlineVersion)
+        {
+            State = GameState.downloading;
+            try
+            {
+                WebClient webClient = new WebClient();
+                _onlineVersion = new Version(webClient.DownloadString(downloadVersionUrl).TrimStart('v'));
+
+                Directory.CreateDirectory(downloadPath);
+
+                FileDownloader downloader = new FileDownloader();
+                downloader.DownloadProgressChanged += new FileDownloader.DownloadProgressChangedEventHandler(UpdateProgressCallback);
+                downloader.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadCompletedCallback);
+                downloader.DownloadFileAsync(downloadUrl, Path.Combine(downloadPath, gameName + _onlineVersion + ".zip"), _onlineVersion);
+            }
+            catch (Exception ex)
+            {
+                State = GameState.failed;
+                MessageBox.Show($"Error downloading game: {ex}");
+            }
+        }
+
+
+        private void UpdateProgressCallback(object sender, FileDownloader.DownloadProgress e)
+        {
+            DownloadSize = e.TotalBytesToReceive;
+            DownloadSizeCurrent = e.BytesReceived;
+
+            ProgressText = "Progress: " + e.BytesReceived + "/" + e.TotalBytesToReceive + " (" + e.ProgressPercentage + "%)";
+        }
+
+        private void DownloadCompletedCallback(object sender, AsyncCompletedEventArgs e)
+        {
+            ProgressText = "";
+            InstallGame((Version)e.UserState);
+        }
+
+        private async void InstallGame(Version onlineVersion)
+        {
+            State = GameState.installing;
+
+            try
+            {
+                string zipPath = Path.Combine(downloadPath, gameName + onlineVersion + ".zip");
+
+                if (Directory.Exists(gamePath)) Directory.Delete(gamePath, true);
+                await Task.Run(() => ZipFile.ExtractToDirectory(zipPath, gamesPath));
+                File.Delete(zipPath);
+
+                File.WriteAllText(versionFile, onlineVersion.ToString());
+                LocalVersion = onlineVersion;
+                State = GameState.clickPlay;
+            }
+            catch (Exception ex)
+            {
+                State = GameState.failed;
+                MessageBox.Show($"Error installing game: {ex}");
+            }
+        }
+
+        private ICommand _playCommand;
+        public ICommand PlayCommand => _playCommand ?? (_playCommand = new CommandHandler(() => Play(), () => true));
+
+        public void Play()
+        {
+            switch (State)
+            {
+                case GameState.clickPlay:
+                    ProcessStartInfo processStartInfo = new ProcessStartInfo()
+                    {
+                        FileName = gameExe
+                    };
+                    _ = Process.Start(processStartInfo);
+                    break;
+                case GameState.clickInstall:
+                case GameState.clickUpdate:
+                    State = GameState.downloading;
+                    DownloadGame(new Version(0, 0));
+                    break;
+                case GameState.failed:
+                    CheckUpdates();
+                    break;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyname = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyname));
+        }
+    }
+}
--- TacticalLauncher/MainWindow.xaml.cs
+++ TacticalLauncher/MainWindow.xaml.cs
@@ -1,253 +1,61 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
-using System.Net;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Navigation;
 
 namespace TacticalLauncher
 {
-    enum LauncherStatus
-    {
-        ready,
-        failed,
-        downloadingGame,
-        installingGame,
-        downloadingUpdate
-    }
-
     public partial class MainWindow : Window
     {
-        const string downloadUrlVersion = "https://drive.google.com/uc?export=download&id=18FkOPeDDzqPPgmRb4XdzEHEchfM5U3HV";
-        const string downloadUrlGame = "https://drive.google.com/uc?export=download&id=1FJL0sBPvt5AEdbkgcKshO15Vv-kuJ6gd";
-        const string downloadUrlLauncher = "https://tacticalmath.games/download";
+        private const string downloadUrlLauncher = "https://tacticalmath.games/download";
 
-        string rootPath;
-        string versionFile;
-        string gameZip;
-        string gameExe;
-        string gamePath;
-
-        Version localVersion;
-
-        LauncherStatus _status;
-        internal LauncherStatus Status
-        {
-            get => _status;
-            set
-            {
-                _status = value;
-                switch (_status)
-                {
-                    case LauncherStatus.ready:
-                        PlayButton.Content = "Play";
-                        PlayButton.IsEnabled = true;
-                        ProgressBar.Value = ProgressBar.Maximum;
-                        break;
-                    case LauncherStatus.failed:
-                        PlayButton.Content = "Update Failed - Retry";
-                        PlayButton.IsEnabled = true;
-                        break;
-                    case LauncherStatus.downloadingGame:
-                        PlayButton.Content = "Downloading Game...";
-                        PlayButton.IsEnabled = false;
-                        break;
-                    case LauncherStatus.installingGame:
-                        PlayButton.Content = "Installing Game...";
-                        PlayButton.IsEnabled = false;
-                        break;
-                    case LauncherStatus.downloadingUpdate:
-                        PlayButton.Content = "Downloading Update...";
-                        PlayButton.IsEnabled = false;
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
+        readonly Game tmr = new Game(
+            "https://drive.google.com/uc?export=download&id=1FJL0sBPvt5AEdbkgcKshO15Vv-kuJ6gd",
+            "https://drive.google.com/uc?export=download&id=18FkOPeDDzqPPgmRb4XdzEHEchfM5U3HV",
+            "TacticalMathReturns", "TacticalMathReturns.exe");
+        readonly Game md2 = new Game(
+            "",
+            "",
+            "MothershipDefender2", "MothershipDefender2.exe");
 
         public MainWindow()
         {
             InitializeComponent();
             VersionTextLauncher.Text = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
 
-            rootPath = Directory.GetParent(Directory.GetCurrentDirectory()).ToString();
-            versionFile = Path.Combine(rootPath, "TacticalMathReturns.version");
-            gameZip = Path.Combine(rootPath, "TacticalMathReturns.zip");
-            gamePath = Path.Combine(rootPath, "TacticalMathReturns");
-            gameExe = Path.Combine(gamePath, "TacticalMathReturns.exe");
+            TmrTab.DataContext = tmr;
+            MD2Tab.DataContext = md2;
         }
 
-        void SquirrelLauncherUpdates()
+        private void SquirrelLauncherUpdates()
         {
-            string squirrel = Path.Combine(rootPath, "Update.exe");
+            string squirrel = Path.Combine(Directory.GetParent(Directory.GetCurrentDirectory()).ToString(), "Update.exe");
             if (File.Exists(squirrel))
             {
-                var processStartInfo = new ProcessStartInfo()
+                ProcessStartInfo processStartInfo = new ProcessStartInfo()
                 {
                     FileName = squirrel,
                     Arguments = $"--update={downloadUrlLauncher}"
                 };
-                Process.Start(processStartInfo);
+                _ = Process.Start(processStartInfo);
             }
         }
 
-        async void CheckForUpdates()
+        private void Window_ContentRendered(object sender, EventArgs e)
         {
-            await Task.Run(() => SquirrelLauncherUpdates());
-
-            if (File.Exists(versionFile) && File.Exists(gameExe))
-            {
-                await Task.Run(() => localVersion = new Version(File.ReadAllText(versionFile)));
-                VersionTextGame.Text = "v" + localVersion.ToString();
-
-                try
-                {
-                    WebClient webClient = new WebClient();
-                    webClient.DownloadStringCompleted += new DownloadStringCompletedEventHandler(DownloadVersionFileCompletedCallback);
-                    webClient.DownloadStringAsync(new Uri(downloadUrlVersion));
-                }
-                catch (Exception ex)
-                {
-                    Status = LauncherStatus.failed;
-                    MessageBox.Show($"Error checking for updates: {ex}");
-                }
-            }
-            else
-            {
-                DownloadGame(false, new Version(0, 0));
-            }
+            _ = Task.Run(() => SquirrelLauncherUpdates());
         }
 
-        void DownloadGame(bool _isUpdate, Version _onlineVersion)
+        private void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
         {
-            try
-            {
-                WebClient webClient = new WebClient();
-                if (_isUpdate)
-                {
-                    Status = LauncherStatus.downloadingUpdate;
-                }
-                else
-                {
-                    Status = LauncherStatus.downloadingGame;
-                    _onlineVersion = new Version(webClient.DownloadString(downloadUrlVersion).TrimStart('v'));
-                }
-
-                FileDownloader fileDownloader = new FileDownloader();
-                fileDownloader.DownloadProgressChanged += new FileDownloader.DownloadProgressChangedEventHandler(UpdateProgressText);
-                fileDownloader.DownloadFileCompleted += new AsyncCompletedEventHandler(DownloadGameCompletedCallback);
-                fileDownloader.DownloadFileAsync(downloadUrlGame, gameZip, _onlineVersion);
-            }
-            catch (Exception ex)
-            {
-                Status = LauncherStatus.failed;
-                MessageBox.Show($"Error downloading game: {ex}");
-            }
-        }
-
-        void UpdateProgressText(object sender, FileDownloader.DownloadProgress e)
-        {
-            ProgressBar.Minimum = 0;
-            ProgressBar.Maximum = e.TotalBytesToReceive;
-
-            if (e.TotalBytesToReceive > 0 && e.TotalBytesToReceive != e.BytesReceived)
-            {
-                ProgressBar.Value = e.BytesReceived;
-                Progress.Text = "Progress: " + e.BytesReceived + "/" + e.TotalBytesToReceive + " (" + e.ProgressPercentage + "%)";
-            }
-            else
-            {
-                Progress.Text = "Progress: " + e.BytesReceived;
-            }
-        }
-
-        void DownloadVersionFileCompletedCallback(object sender, DownloadStringCompletedEventArgs e)
-        {
-            try
-            {
-                Version onlineVersion = new Version(e.Result.TrimStart('v'));
-
-                if (onlineVersion != localVersion)
-                {
-                    DownloadGame(true, onlineVersion);
-                }
-                else
-                {
-                    Status = LauncherStatus.ready;
-                }
-            }
-            catch (Exception ex)
-            {
-                Status = LauncherStatus.failed;
-                MessageBox.Show($"Error checking for updates: {ex}");
-            }
-        }
-
-        void DownloadGameCompletedCallback(object sender, AsyncCompletedEventArgs e)
-        {
-            string onlineVersion = ((Version)e.UserState).ToString();
-            InstallGame(onlineVersion);
-        }
-
-        async void InstallGame(string onlineVersion)
-        {
-            Status = LauncherStatus.installingGame;
-            Progress.Text = "";
-            try
-            {
-                if (Directory.Exists(gamePath)) Directory.Delete(gamePath, true);
-                await Task.Run(() => ZipFile.ExtractToDirectory(gameZip, rootPath));
-                File.Delete(gameZip);
-
-                File.WriteAllText(versionFile, onlineVersion);
-
-                VersionTextGame.Text = "v" + onlineVersion;
-                Status = LauncherStatus.ready;
-            }
-            catch (Exception ex)
-            {
-                Status = LauncherStatus.failed;
-                MessageBox.Show($"Error installing game: {ex}");
-            }
-        }
-
-        void Window_ContentRendered(object sender, EventArgs e)
-        {
-            CheckForUpdates();
-        }
-
-        void PlayButton_Click(object sender, RoutedEventArgs e)
-        {
-            if (File.Exists(gameExe) && Status == LauncherStatus.ready)
-            {
-                var processStartInfo = new ProcessStartInfo()
-                {
-                    FileName = gameExe,
-                    WorkingDirectory = Path.Combine(rootPath, "TMR")
-                };
-                Process.Start(processStartInfo);
-
-                Close();
-            }
-            else if (Status == LauncherStatus.failed)
-            {
-                CheckForUpdates();
-            }
-        }
-
-        void Hyperlink_RequestNavigate(object sender, RequestNavigateEventArgs e)
-        {
-            var processStartInfo = new ProcessStartInfo()
+            ProcessStartInfo processStartInfo = new ProcessStartInfo()
             {
                 FileName = e.Uri.AbsoluteUri,
                 UseShellExecute = true
             };
-            Process.Start(processStartInfo);
-
+            _ = Process.Start(processStartInfo);
             e.Handled = true;
         }
     }
